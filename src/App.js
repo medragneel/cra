e8d68e78f13d53a6829989aa45cd9fb2804a5fea
mshadow <medmystgon@gmail.com>
2022-09-22 19:58:40 +0100

    cra

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './css/app.css';
+
+function App() {
+  return (
+    <div className="fs-2">
+      <center>
+      <h1>
+      hello medmh
+      </h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
